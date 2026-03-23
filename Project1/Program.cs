7f6d20cc5b7b4bb66158d21c69e537cc58ec6a5e
test name <test email>
2026-03-23 21:32:09 +0200

    add to Program.cs

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -6,54 +6,69 @@ namespace Project1
     {
         static void Main(string[] args)
         {
-            //Process current = Process.GetCurrentProcess();
-            //current.PriorityClass = ProcessPriorityClass.RealTime;
-            //Console.WriteLine("---- Process Info -----");
-            //Console.WriteLine($"Priority Class: {current.PriorityClass}");
-            //Console.WriteLine($"Name: {current.ProcessName}");
-            //Console.WriteLine($"Id: {current.Id}");
-            //Console.WriteLine($"MachineName: {current.MachineName}");
-            //Console.WriteLine($"PrivateMemorySize: {current.PrivateMemorySize}");
-            //Console.WriteLine($"StartTime: {current.StartTime}");
-            //Console.WriteLine($"TotalProcessorTime: {current.TotalProcessorTime}");
-            //Console.WriteLine($"UserProcessorTime: {current.UserProcessorTime}");
+            //Process process = new Process();    
 
-            //Process[] processes = Process.GetProcesses();
-            //Console.WriteLine("Name\t\t\tPID\t\t\tPriority\t\t\tMachine name");
-            //foreach (var p in processes)
-            //{
-            //    try
-            //    {
-            //        Console.WriteLine($"{p.ProcessName}\t" +
-            //        $"{p.Id}\t{p.PriorityClass}\t{p.MachineName}");
-            //    }
-            //    catch (Exception ex)
-            //    {
-            //        Console.ForegroundColor = ConsoleColor.Red;
-            //        Console.WriteLine(p.ProcessName);
-            //        Console.ResetColor();
-            //    }
+            /*Process current = Process.GetCurrentProcess();
+            current.PriorityClass = ProcessPriorityClass.High;
+            Console.WriteLine("------Current process info");
+            Console.WriteLine($"Priority Class : {current.PriorityClass}");
+            Console.WriteLine($"Name : {current.ProcessName}");
+            Console.WriteLine($"Id : {current.Id}");
+            Console.WriteLine($"Machine Name : {current.MachineName}");
+            Console.WriteLine($"Private memoory size : {current.PrivateMemorySize64/1024}");
+            Console.WriteLine($"Start time : {current.StartTime}");
+            Console.WriteLine($"TotalProcessorTime : {current.TotalProcessorTime}");
+            Console.WriteLine($"UserProcessorTime : {current.UserProcessorTime}");
+            */
 
-            //}
+            /*
+            Process[] processes = Process.GetProcesses();
+            Console.WriteLine("Name\t\t\tPID\t\t\tPriority\t\t\tMachine name");
+            foreach (var p in processes)
+            {
+                try
+                {
+                    Console.WriteLine($"{p.ProcessName}\t" +
+                    $"{p.Id}\t{p.PriorityClass}\t{p.MachineName}\t" +
+                    $"{p.StartTime}");
 
-            //Process.Start(@"C:\Program Files\Google\Chrome\Application\chrome.exe");
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red; 
+                    Console.WriteLine(p.ProcessName);
+                    Console.ResetColor();
+                }
+                
+            }
+            */
+
+            //Process.Start("MicrosoftEdge.exe", "google.com stackoverflow.com");
+            //Process.Start("Calc.exe");
+            //Process.Start(@"C:\Program Files\Google\Chrome\Application\chrome.exe","google.com stackoverflow.com");
+
+
             ProcessStartInfo info = new ProcessStartInfo()
             {
                 FileName = @"notepad",
-                Arguments = $@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}",
+                Arguments = $@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\TestProcess.txt",
                 WindowStyle = ProcessWindowStyle.Maximized
             };
 
             Process pr = Process.Start(info);
             Console.ReadKey();
-            //pr.Kill();
-            pr.CloseMainWindow();
-            //pr.Close();
-            Console.WriteLine("Wait for exit.......");
+            //pr.Kill();//End TAsk
+            //pr.CloseMainWindow();//Alt+F4
+            //pr.Close();//clear reference
+
+            Console.WriteLine("Wait for exit....");
+
             pr.WaitForExit();
-            Console.WriteLine($"Exit code: {pr.ExitCode}");
-            Console.WriteLine($"Exit code: {pr.ExitTime}");
+            Console.WriteLine($"Exit code : {pr.ExitCode}");
+            Console.WriteLine($"Exit time : {pr.ExitTime}");
+
             Console.ReadKey();
-        }
+        
+        }   
     }
 }
