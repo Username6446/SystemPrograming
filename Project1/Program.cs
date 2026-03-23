448c0c8b2c51c5524226acdc81d2cd738ae9723b
test name <test email>
2026-03-23 21:30:30 +0200

    Add project files.

---
+++ Project1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+
+namespace Project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Process current = Process.GetCurrentProcess();
+            //current.PriorityClass = ProcessPriorityClass.RealTime;
+            //Console.WriteLine("---- Process Info -----");
+            //Console.WriteLine($"Priority Class: {current.PriorityClass}");
+            //Console.WriteLine($"Name: {current.ProcessName}");
+            //Console.WriteLine($"Id: {current.Id}");
+            //Console.WriteLine($"MachineName: {current.MachineName}");
+            //Console.WriteLine($"PrivateMemorySize: {current.PrivateMemorySize}");
+            //Console.WriteLine($"StartTime: {current.StartTime}");
+            //Console.WriteLine($"TotalProcessorTime: {current.TotalProcessorTime}");
+            //Console.WriteLine($"UserProcessorTime: {current.UserProcessorTime}");
+
+            //Process[] processes = Process.GetProcesses();
+            //Console.WriteLine("Name\t\t\tPID\t\t\tPriority\t\t\tMachine name");
+            //foreach (var p in processes)
+            //{
+            //    try
+            //    {
+            //        Console.WriteLine($"{p.ProcessName}\t" +
+            //        $"{p.Id}\t{p.PriorityClass}\t{p.MachineName}");
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        Console.ForegroundColor = ConsoleColor.Red;
+            //        Console.WriteLine(p.ProcessName);
+            //        Console.ResetColor();
+            //    }
+
+            //}
+
+            //Process.Start(@"C:\Program Files\Google\Chrome\Application\chrome.exe");
+            ProcessStartInfo info = new ProcessStartInfo()
+            {
+                FileName = @"notepad",
+                Arguments = $@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}",
+                WindowStyle = ProcessWindowStyle.Maximized
+            };
+
+            Process pr = Process.Start(info);
+            Console.ReadKey();
+            //pr.Kill();
+            pr.CloseMainWindow();
+            //pr.Close();
+            Console.WriteLine("Wait for exit.......");
+            pr.WaitForExit();
+            Console.WriteLine($"Exit code: {pr.ExitCode}");
+            Console.WriteLine($"Exit code: {pr.ExitTime}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Project1_DZ_TaskManager/MainWindow.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project1_DZ_TaskManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            grid.ItemsSource = Process.GetProcesses();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (grid.SelectedItem is Process selected)
+            {
+                selected.Kill();
+            }
+            else
+            {
+                MessageBox.Show("Select one item from table!");
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+            if (grid.SelectedItem is Process selected)
+            {
+                MessageBox.Show($"{selected.ProcessName} Select one item from table!");
+                bool falg = selected.CloseMainWindow();
+                MessageBox.Show($"{falg}123123");
+            }
+            else
+            {
+                MessageBox.Show("Select one item from table!");
+            }
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            if (grid.SelectedItem is Process selected)
+            {
+                StringBuilder info = new StringBuilder();
+
+                try
+                {
+                    info.AppendLine("---- Process Info -----");
+                    info.AppendLine($"Name: {selected.ProcessName}");
+                    info.AppendLine($"Id: {selected.Id}");
+                    info.AppendLine($"Priority Class: {selected.PriorityClass}");
+                    info.AppendLine($"Machine Name: {selected.MachineName}");
+                    info.AppendLine($"Private Memory: {selected.PrivateMemorySize64 / 1024 / 1024} MB");
+                    info.AppendLine($"Start Time: {selected.StartTime}");
+                    info.AppendLine($"Total CPU Time: {selected.TotalProcessorTime}");
+                    info.AppendLine($"User CPU Time: {selected.UserProcessorTime}");
+
+                    MessageBox.Show(info.ToString(), "Process Details", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Access Denied:\nDetails: {ex.Message}",
+                                    "System Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Select one item from table!");
+            }
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            string nameToStart = processName.Text;
+            if (!string.IsNullOrWhiteSpace(nameToStart))
+            {
+                try
+                {
+                    Process.Start(new ProcessStartInfo(nameToStart) { UseShellExecute = true });
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error: {ex.Message}");
+                }
+            }
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            if (grid.SelectedItem is Process selected)
+            {
+                string nameProcessToStart = processName.Text;
+                MessageBox.Show($"{selected.ProcessName} \t{nameProcessToStart}", "Some text", MessageBoxButton.OKCancel, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("Select one item from table!");
+            }
+        }
+    }
+}
