35b76458a16af79527d59552220faa803d883b5b
test name <test email>
2026-03-25 20:48:35 +0200

    DZ_02

---
+++ Project2/Program.cs
@@ -0,0 +1,126 @@
+﻿namespace Project2
+{
+    internal class Program
+    {
+        static void Method()
+        {
+            Console.WriteLine($"Id of method thread : {Thread.CurrentThread.GetHashCode()}");
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine($"\t\t\t{i} - Hello in thread.");
+                Thread.Sleep(50);
+            }
+        }
+        static void MethodWithParams(object? a)
+        {
+            //Tuple<int, int> tuple = new Tuple<int, int>(25, 50);
+
+            string name = (string)a!;
+            //Console.WriteLine(name);
+            //string name2 = a?.ToString()!;
+            //Console.WriteLine(name);
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine($"\t\t\t{i} - {name}.");
+                //Thread.Sleep(50);
+            }
+        }
+        static void Main(string[] args)
+        {
+            #region Example 1
+            //int a = 5;
+            //MethodWithParams(a);
+            ///string name = "Ira";
+            //MethodWithParams(name);
+            //ParameterizedThreadStart
+            //ThreadStart start = new ThreadStart(Method);
+            //Thread thread = new Thread(start);
+            //Thread thread = new Thread(new ThreadStart(Method));
+            //Thread thread = new Thread(Method);
+            //thread.Start();
+            //Method();
+            #endregion
+
+
+
+            #region Example 2
+            /*
+             * Thread Priority
+             * Highest
+             * AboveNormal
+             * Normal
+             * BelowNormal
+             * Lowest
+             
+             */
+            //Thread thread1 = new Thread(MethodWithParams);
+            //thread1.Priority = ThreadPriority.Lowest;
+            //thread1.Start("One");
+
+            //Thread thread2 = new Thread(MethodWithParams);
+            //thread2.Priority = ThreadPriority.Highest;
+            //thread2.Start("\t\tTwo");
+
+            ////Priority (Normal)
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    Console.WriteLine($"{i} - Hello in main.");
+            //}
+            #endregion
+
+
+
+            #region Example 3
+            //Thread thread = new Thread(Method); 
+            //thread.IsBackground = true;
+            //thread.Start();
+
+            ////thread.Suspend();
+            ////thread.Resume();
+            ////thread.Abort();//close thread
+
+            //Console.WriteLine($"Id of main thread : {Thread.CurrentThread.GetHashCode()}");
+            //Console.ReadKey();
+            //Console.WriteLine("Main thread");
+
+            #endregion
+
+
+            #region TestThreads
+            //ConsoleKeyInfo input;
+            //do 
+            //{ 
+            //    input = Console.ReadKey();
+            //    Thread thread = new Thread(InfinityLoop);
+            //    thread.Start();
+            //    //InfinityLoop();
+
+            //} while (input.Key != ConsoleKey.Escape);
+
+
+            #endregion
+
+            #region Wait Thread
+            Thread thread = new Thread(Method);
+            Console.WriteLine("Thread is going start.....");
+            thread.Start();
+
+            Thread.Sleep(200);
+            Console.WriteLine("Waiting for the end thread");
+            thread.Join();
+            Console.WriteLine("Program was ended...");
+            #endregion
+        }
+        static void InfinityLoop()
+        {
+            Console.WriteLine("Thread has been started.");
+            while (true)
+            {
+                int a = 5;
+                int b = 7;
+                int c = a + b;
+                new Random().Next(100);
+            }
+        }
+    }
+}
---
+++ Project2_DZ_Threads/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace Project2_DZ_Threads
+{
+    internal class Program
+    {
+        static void Ex1()
+        {
+            for (int i = 1; i <= 50; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        static void Ex2(object? a)
+        {
+            var data = (Tuple<int, int>)a!;
+            for (int i = data.Item1; i <= data.Item2; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        static void Ex3(object? a)
+        {
+            var data = (Tuple<int, int, int>)a!;
+            for(int i = 0; i<data.Item3;i++)
+            {
+                Thread thread = new Thread(Ex2);
+                thread.Start((data.Item1, data.Item2));
+
+            }
+            
+        }
+
+        static void MinArr(object? a)
+        {
+            int[] arr = (int[])a!;
+            Console.WriteLine($"Min : {arr.Min()}");
+
+        }
+        static void MaxArr(object? a)
+        {
+            int[] arr = (int[])a!;
+            Console.WriteLine($"Max : {arr.Max()}");
+
+        }
+        static void AvgArr(object? a)
+        {
+            int[] arr = (int[])a!;
+            Console.WriteLine($"Avg : {arr.Average()}");
+
+        }
+        static void PrintArr(object? a)
+        {
+            int[] arr = (int[])a!;
+            foreach (var item in arr)
+            {
+                Console.WriteLine($"Element of arr {item}");
+            }
+        }
+        static void PrintFileArr(object? a)
+        {
+            int[] arr = (int[])a!;
+            File.AppendAllLines("Arr.txt", arr.Select(x => x.ToString()));
+        }
+        static void Ex4_5(object? a)
+        {
+
+            int[] arr = new int[100];
+            for (int i = 0; i < 100; i++)
+            {
+                arr[i] = new Random().Next(100);
+            }
+            Thread thread1 = new Thread(PrintArr);
+            thread1.Start(arr);
+            Thread thread2 = new Thread(MinArr);
+            thread2.Start(arr);
+            Thread thread3 = new Thread(MaxArr);
+            thread3.Start(arr);
+            Thread thread4 = new Thread(AvgArr);
+            thread4.Start(arr);
+            Thread thread5 = new Thread(PrintFileArr);
+            thread5.Start(arr);
+        }
+        static void Main(string[] args)
+        {
+
+            //Thread thread = new Thread(Ex1);
+            //thread.Start();
+
+            //Tuple<int, int> tuple = new Tuple<int, int>(25, 50);
+            //Thread thread = new Thread(Ex2);
+            //thread.Start(tuple);
+
+            //Tuple<int, int, int> tuple = new Tuple<int, int, int>(25, 50, 4);
+            //Thread thread = new Thread(Ex3);
+            //thread.Start(tuple);
+
+            Thread thread = new Thread(Ex4_5);
+            thread.Start();
+
+        }
+    }
+}
